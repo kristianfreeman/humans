749682bb92f82d4517e93ede1e50164fcb5275d1
Kristian Freeman <kristianfreeman@gmail.com>
2012-06-08 11:34:00 -0700

    Generator work

--- lib/humans.rb
+++ lib/humans.rb
@@ -3,22 +3,23 @@ require "humans/version"
 module Humans
 
   def self.generate
-    people = `git log --format='%aN|%aE' | sort -u`.split
+    people = `git log --format='%aN|%aE' | sort -u`.split("\n")
 
     File.open('humans.txt', 'w') do |f|
 
-      f = <<-eos
-      /* TEAM */
+      f << <<-eos
+/* TEAM */
 
 
-      eos
+eos
 
       people.each do |w|
-        name, *email = w.split("|")
+        name, *email = w.to_s.split("|")
+        email.to_s.gsub!(/\[|\"\]/, "")
 
         f << <<-eos
-        Name:  #{ name }
-        Email: #{ email} 
+Name:  #{ name }
+Email: #{ email } 
 
         eos
 
